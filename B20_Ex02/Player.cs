dd5ab054967c9dc43cbb3e40301de9b37150d9c7
DanielK-14 <dany,kalmikov@gmail.com>
2020-05-24 23:38:08 +0300

    1

--- B20_Ex02/Player.cs
+++ B20_Ex02/Player.cs
@@ -6,5 +6,12 @@ namespace B20_Ex02
 {
     class Player
     {
+        private readonly string m_Name;
+        private int m_Score;
+
+        public Player(string i_Name)
+        {
+
+        }
     }
 }
